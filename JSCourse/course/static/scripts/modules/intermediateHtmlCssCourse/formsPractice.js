db1ca93df3e5733c05520f8fc29c572641ef14bb
Kamil Sidor <kamil_sidor@o2.pl>
2023-08-06 17:10:36 +0200

    Add partial visual validation.
    JS for validating password controls works as intended. Working on
    ::after pseudo elements for the form controls co convey validity
    by some other means than color only.

--- JSCourse/course/static/scripts/modules/intermediateHtmlCssCourse/formsPractice.js
+++ JSCourse/course/static/scripts/modules/intermediateHtmlCssCourse/formsPractice.js
@@ -1,4 +1,39 @@
 const FormsPractice = (function() {
+  const passwordField = document.getElementById('password');
+  const confirmField  = document.getElementById('confirm' );
+
+  const passFields = [passwordField, confirmField];
+
+  function validatePassword() {
+    let password = passwordField.value;
+    let confirm  = confirmField .value;
+
+    console.log(password, confirm);
+
+    passFields.forEach(
+      field => {
+        field.setCustomValidity(""); // Valid by default;
+
+        if (field.value.length < 6) {
+          field.setCustomValidity("Passwords must be at least 6 characters long");
+        }
+      }
+    );
+    
+    if (password !== confirm) {
+      passFields.forEach(
+        field => field.setCustomValidity("Passwords don't match")
+      );
+    }
+
+    
+  }
+
+  passFields.forEach(field => {
+    field.addEventListener("input", validatePassword);
+  })
+
+
   function run() {
     console.log("Forms practice module loaded!");
   }

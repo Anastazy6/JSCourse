e649db37474bd86555eb3d151f56835d7d166e3a
Kamil Sidor <kamil_sidor@o2.pl>
2023-08-24 15:05:25 +0200

    Finish section 'Loading Images'

--- WebpackTest/src/index.js
+++ WebpackTest/src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import myName from './myName';
 import './style.css';
+import myIcon from './Ikona.png';
 
 function component() {
   const element = document.createElement('div');
@@ -8,7 +9,17 @@ function component() {
 
   element.textContent = myName("Mephistopheles");
 
+  
   return element;
 }
 
+
+function Icon() {
+  const icon = new Image();
+  icon.src = myIcon;
+
+  return icon;
+}
+
 document.body.appendChild(component());
+document.body.appendChild(Icon());

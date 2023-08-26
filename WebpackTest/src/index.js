9cbbf2d72d2443ff4a206a85c4c1b9412cad1c1b
Kamil Sidor <kamil_sidor@o2.pl>
2023-08-26 15:21:52 +0200

    Test HTML Webpack plugin

--- WebpackTest/src/index.js
+++ WebpackTest/src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
-import myName from './myName';
-
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
-  element.classList.add('hello');
+  const btn = document.createElement('button');
 
-  element.textContent = myName("Mephistopheles");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 

985f3928f208b70b4e64cc9b327d91c861cdec03
Kamil Sidor <kamil_sidor@o2.pl>
2023-08-26 14:57:36 +0200

    Purge contents to prepare for Output management Webpack course

--- WebpackTest/src/index.js
+++ WebpackTest/src/index.js
@@ -1,21 +1,6 @@
 import _ from 'lodash';
 import myName from './myName';
-import './style.css';
-import myIcon from './Ikona.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
@@ -27,16 +12,4 @@ function component() {
   return element;
 }
 
-
-function Icon() {
-  const icon = new Image();
-  icon.src = myIcon;
-
-  return icon;
-}
-
-console.log(Data);
-console.log(Notes);
-
 document.body.appendChild(component());
-// document.body.appendChild(Icon());

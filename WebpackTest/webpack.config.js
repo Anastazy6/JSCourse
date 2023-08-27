a742b18c15e0b60845526702c481324723b28a85
Kamil Sidor <kamil_sidor@o2.pl>
2023-08-27 16:29:34 +0200

    Test several code-splitting techniques

--- WebpackTest/webpack.config.js
+++ WebpackTest/webpack.config.js
@@ -4,8 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   devtool: 'source-map',
   devServer: {
@@ -16,12 +23,13 @@ module.exports = {
       title: 'Development',
     }),
   ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
+    publicPath: '/',
   },
 };

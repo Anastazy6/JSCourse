7846e591dce051f2e2b1d002afa73bd34202793a
Kamil Sidor <kamil_sidor@o2.pl>
2023-09-02 17:27:23 +0200

    Add contents, parsing and styles to the Menu page

---
+++ Restaurant/src/Menu/contents.js
@@ -0,0 +1,77 @@
+const Contents = {
+  Spirits: [
+    {
+      name : "Captain Morgan Spiced Gold",
+      info : 'Rum (spiced)',
+      price: 10,
+      vol  : 50,
+      power: 36
+    }, {
+      name : "Jack Daniel's",
+      info : "Whisky",
+      price: 12,
+      vol  : 50,
+      power: 40
+    }, {
+      name : "Grandpa's basement special",
+      info : "Moonshine (homemade)",
+      price: 16,
+      vol  : 50,
+      power: 80
+    }, {
+      name : "Jameson",
+      info : "Whisky",
+      price: 12,
+      vol  : 50,
+      power: 40
+    }, {
+      name : "Sakramencko",
+      info : "Moonshine (plum flavor)",
+      price: 20,
+      vol  : 50,
+      power: 80
+    }, {
+      name : "Ogiński",
+      info : "Vodka",
+      price: 8,
+      vol  : 50,
+      power: 40
+    }, {
+      name : "Captain Morgan Spiced Premium Black",
+      info : "Rum (black, spiced)",
+      price: 18,
+      vol  : 50,
+      power: 40
+    }
+  ],
+  Drinks: [
+    {
+      name : "Rum with cola",
+      info : "Rum 50ml, cola 200ml",
+      price: 20,
+      vol  : 250,
+      power: 8
+      
+    }, {
+      name : "Wodka with cola",
+      info : "Wodka 50ml, cola 200ml",
+      price: 18,
+      vol  : 250,
+      power: 8
+    }, {
+      name : "Whisky with cola",
+      info : "Whisky 50ml, cola 200ml",
+      price: 22,
+      vol  : 250,
+      power: 8
+    }, {
+      name : "Death of thousand vomits",
+      info : "Rum, whisky, vodka, gin, tequila, 50ml of each",
+      price: 55,
+      vol  : 250,
+      power: 40
+    }
+  ]
+}
+
+export default Contents;
--- Restaurant/src/Menu/menu.js
+++ Restaurant/src/Menu/menu.js
@@ -1,14 +1,61 @@
+import Contents from "./contents";
+
 function Menu() {
   const menu     = document.createElement('div');
   const fragment = document.createDocumentFragment();
 
-  const test = document.createElement('h1');
-  test.innerText = "Menu page loaded!";
+  fragment.appendChild(MenuHeader());
 
-  fragment.appendChild(test);
-  menu    .appendChild(fragment);
+  Object.entries(Contents).map(([group, drinks]) => {
+    fragment.appendChild(createMenuGroup(group, drinks));
+  })
+
+  menu.appendChild(fragment);
 
   return menu;
 }
+
+function MenuHeader() {
+  const header = document.createElement('h1');
+  header.innerText = 'Menu';
+
+  return header;
+}
+
+
+function createMenuGroup(group, drinks) {
+  const menuGroup = document.createElement('section');
+  const fragment  = document.createDocumentFragment();
+  const header    = document.createElement('h2');
+
+  header.innerText = group;
+  fragment.appendChild(header);
+
+  drinks.map(drink => fragment.appendChild(createDrink(drink)));
+
+  menuGroup.appendChild(fragment);
+  return menuGroup;
+}
+
+
+function createDrink(drinkData) {
+  const drink    = document.createElement('article');
+  const fragment = document.createDocumentFragment();
+
+  Object.entries(drinkData).map(([key, value]) => {
+    const field = document.createElement(key === 'name' ? 'h3' : 'div');
+    
+    field.classList.add(`drink-${key}`);
+    field.innerText = value;
+
+    fragment.appendChild(field);
+  })
+
+  drink.classList.add('menu-drink');
+  drink.appendChild(fragment);
+
+  return drink;
+}
+
 
 export default Menu;

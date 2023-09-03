38a93c80eb36ca3fedb0bc3f75f8cc70b181af5d
Kamil Sidor <kamil_sidor@o2.pl>
2023-09-03 17:35:50 +0200

    Purge the commented out code from menu.js

--- Restaurant/src/Menu/menu.js
+++ Restaurant/src/Menu/menu.js
@@ -26,39 +26,4 @@ function Menu() {
 }
 
 
-
-
-// function createMenuGroup(group, drinks) {
-//   const menuGroup = document.createElement('section');
-//   const fragment  = document.createDocumentFragment();
-  
-//   fragment.appendChild(Header('h2', group));
-
-//   drinks.map(drink => fragment.appendChild(createDrink(drink)));
-
-//   menuGroup.appendChild(fragment);
-//   return menuGroup;
-// }
-
-
-// function createDrink(drinkData) {
-//   const drink    = document.createElement('article');
-//   const fragment = document.createDocumentFragment();
-
-//   Object.entries(drinkData).map(([key, value]) => {
-//     const field = document.createElement(key === 'name' ? 'h3' : 'div');
-    
-//     field.classList.add(`drink-${key}`);
-//     field.innerText = value;
-
-//     fragment.appendChild(field);
-//   })
-
-//   drink.classList.add('menu-drink');
-//   drink.appendChild(fragment);
-
-//   return drink;
-// }
-
-
 export default Menu;

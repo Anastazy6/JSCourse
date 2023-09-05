224b00ef24bd0a705c77ff208806aee4fb687e60
Kamil Sidor <kamil_sidor@o2.pl>
2023-09-05 17:19:53 +0200

    Start another project: TODO list

---
+++ TODOList/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: './src/scripts/index.js',
+  mode : 'development',
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};

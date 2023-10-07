e6bb8e72ec3eb85285c5bd86b625b44c0d2693f9
Kamil Sidor <kamil_sidor@o2.pl>
2023-10-07 17:47:06 +0200

    Create Node class for Binary Search Tree.
    TODO: create additional tests for the Node class (at the moment there's
    only one test).

--- DataStructures/BST/node.js
+++ DataStructures/BST/node.js
@@ -1,10 +1,43 @@
 class Node {
-  constructor (value=undefined, left=null, right=null) {
-    this.value = value;
-    this.left  = left;
-    this.right = right;
+  constructor (data=undefined, left=null, right=null) {
+    this._data  = data;
+    this._left  = left;
+    this._right = right;
   }
 
+  get data () {
+    return this._data;
+  }
+
+  get left () {
+    return this._left;
+  }
+
+  get right () {
+    return this._right;
+  }
+
+  set data (value) {
+    this._data = value;
+  }
+
+  set left (nextNode) {
+    this.#ensureValidNode(nextNode);
+    this._left = nextNode;
+  }
+
+  set right (nextNode) {
+    this.#ensureValidNode(nextNode);
+    this._right = nextNode;
+  }
+
+  #ensureValidNode (nextNode) {
+    if ( !(nextNode instanceof Node || nextNode === null) ) {
+      throw new TypeError(
+        "The next node must be an instance of the Node class or null"
+      );
+    }
+  }
 }
 
 export default Node;
--- DataStructures/BST/node.test.js
+++ DataStructures/BST/node.test.js
@@ -1,5 +1,12 @@
 import Node from "./node";
 
-describe("It creates a Node for Binary Search Tree", () => {
-  test.todo("It creates a leaf by default");
+describe.only("It creates a Node for Binary Search Tree", () => {
+  test("It creates a leaf by default", () => {
+    let node = new Node('leaf?');
+    
+    expect(node instanceof Node).toBe(true);
+    expect(node.data) .toEqual("leaf?");
+    expect(node.left) .toBe(null);
+    expect(node.right).toBe(null);  
+  });
 });

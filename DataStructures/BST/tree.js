f8881d94331ae85b257d965964f2a6c230ff845a
Kamil Sidor <kamil_sidor@o2.pl>
2023-10-08 20:23:45 +0200

    Write several tests for the BST

--- DataStructures/BST/tree.js
+++ DataStructures/BST/tree.js
@@ -1,9 +1,115 @@
 import Node from "./node";
 
 class Tree {
-  constructor () {
-    // TODO
+  /**
+   * By default, the array on which the tree will be based upon is considered
+   *   unsorted and containing duplicates. If you are sure your array is either
+   *   sorted or has no duplicates, you can pass this information as arguments
+   *   for the constructor, so that less operations are needed to create a BST.
+   *   Use at your own risk: if the array is unsorted or has duplicates, doing so
+   *   will distort the tree.
+   * @param {*} array 
+   * @param {*} unsorted 
+   * @param {*} hasDuplicates 
+   */
+  constructor (array, unsorted=true, hasDuplicates=true) {
+    if (hasDuplicates) array = Array.from(new Set(array));
+    if (unsorted     ) array = array.sort();
+    
+    this.root = this.buildTree(array, 0, array.length - 1);
   }
+
+  /**
+   *  The Odin Project's pretty print
+   *  FIX NEEDED!
+   */
+  static prettyPrint (node, prefix="", isLeft=true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      Tree.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      Tree.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+
+
+  buildTree (array, start, end) { 
+    if (start > end) {
+      return null;
+    }
+
+    let mid = (start + end) / 2;
+    
+    let root   = new Node(array[mid]);
+    root.left  = this.buildTree(array, start,   mid - 1);
+    root.right = this.buildTree(array, mid + 1, end)
+
+    return root;
+  }
+
+  insert (value) {
+
+  }
+
+
+  delete (value) {
+
+  }
+
+
+  find (value) {
+
+    // returns NODE
+  }
+
+
+  levelOrder () {
+
+  }
+
+
+  preorder () {
+
+
+  }
+
+  inorder () {
+
+
+  }
+
+
+  postorder () {
+
+  }
+
+
+  height () {
+
+  }
+
+
+  depth () {
+
+  }
+
+  
+  isBalanced () {
+
+  }
+
+
+  rebalance () {
+
+  }
+
+
+
 }
 
 export default Tree;
--- DataStructures/BST/tree.test.js
+++ DataStructures/BST/tree.test.js
@@ -11,7 +11,7 @@ describe("It creates a Binary Search Tree", () => {
 
   test("It creates a single-node tree", () => {
     let tree = new Tree([1]);
-    expectedRoot = {
+    let expectedRoot = {
       data : 1,
       left : null,
       right: null
@@ -36,18 +36,129 @@ describe("It creates a Binary Search Tree", () => {
       expect(tree.root.data).toBe(2);
       expect(analyzeNode(tree.root.left,  expectedLeft )).toBe(true);
       expect(analyzeNode(tree.root.right, expectedRight)).toBe(true);
+      Tree.prettyPrint(tree.root);
     }
 
 
-    test("Works with a sorted array", () => {
+    test("Works with a sorted, free of duplicates array", () => {
       let tree = new Tree([1, 2, 3]);
       runTests(tree);
     });
 
 
     test("Works with an unsorted array, sorting it at creation", () => {
-      let tree = new Tree([2, 1, 3], false);
+      let tree = new Tree([2, 1, 3]);
       runTests(tree);
+    });
+
+
+    test("Works with a sorted array, but containing duplicates", () => {
+      let tree = new Tree([1, 1, 2, 2, 2, 3, 3, 3]);
+      runTests(tree);
+    });
+
+
+    test("Works with an unsorted array that contains duplicates", () => {
+      let tree = new Tree([3, 1, 2, 3, 1, 2, 3, 3, 2, 3, 2, 1, 2, 3]);
+
+      runTests(tree);
+    });
+  });
+});
+
+
+describe("It inserts a value (as a Node)", () => {
+  test("It inserts a value into an empty tree, creating its root node", () => {
+    let tree = new Tree([]);
+    tree.insert(1);
+    let expectedRoot = {
+      data: 1,
+      left: null,
+      right: null
+    };
+
+    expect(analyzeNode(tree.root, expectedRoot)).toBe(true);
+  });
+
+  // This should alse test whether it picks the correct branch
+  describe("It inserts a value to a root-only tree", () => {
+    test("It inserts a value to the left branch if value < root.data", () => {
+      let tree = new Tree(3);
+      let expectedLeft = {
+        data: 2,
+        left: null,
+        right: null
+      }
+
+      tree.insert(2);
+      expect(analyzeNode(tree.root.left, expectedLeft)).toBe(true);
+      expect(tree.root.right).toBe(null);
+    });
+
+
+    test("It inserts a value to the right branch if value > root.data", () => {
+      let tree = new Tree(3);
+      let expectedRight = {
+        data: 4,
+        left: null,
+        right: null
+      };
+
+      tree.insert(4);
+      expect(analyzeNode(tree.root.right, expectedRight)).toBe(true);
+      expect(tree.root.left).toBe(null);
+    });
+
+
+    test("It does not insert a value that is already present in the tree", () => {
+      let tree = new Tree(3);
+      let expectedRoot = {
+        data: 3,
+        left: null,
+        right: null
+      };
+      expect(analyzeNode(tree.root, expectedRoot)).toBe(true);
+
+      tree.insert(3);
+      expect(analyzeNode(tree.root, expectedRoot)).toBe(true); // Nothing should change
+    });
+  });
+
+  describe("It inserts a value into a leaf node of a bigger tree", () => {
+    
+    /**
+       *              5
+       *           /     \
+       *          3       7 
+       *        /  \    /   \
+       *       2   4   6     8
+       */
+    function makeTestTree () {
+      return new Tree([2, 3, 4, 5, 6, 7, 8]);
+    }
+
+    test("It creates a tree of height 3 (for testing purposes)", () => {
+      let tree = makeTestTree();
+      expect(tree.root.data).toBe(5);
+      expect(tree.root.left.left).toBe(2);
+      expect(tree.root.right).toBe(7);
+      expect(tree.root.right.left).toBe(6);
+      expect(tree.root.left.right.left).toBe(null);
+    });
+
+
+    test("It inserts a value to the far left", () => {
+      let tree = makeTestTree();
+      tree.insert(1);
+      let farLeft = tree.root.left.left.left;
+
+      let expectedFarLeft = {
+        data: 1,
+        left: null,
+        right: null
+      };
+
+      expect(analyzeNode(farLeft, expectedFarLeft)).toBe(true);
     });
   });
 });

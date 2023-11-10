b0000dc12a5324995fe58680b204a54052ec3c74
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-10 20:53:42 +0100

    Rename importance to priority in Constants

--- TODOList/src/Scripts/Constants/limits.js
+++ TODOList/src/Scripts/Constants/limits.js
@@ -2,6 +2,6 @@
 export const MAX_PROJECT_TITLE_LENGTH = 80;
 export const MAX_PROJECT_DESCRIPTION_LENGTH = 1000;
 
-// Project importance limits
-export const MIN_PROJECT_IMPORTANCE = 1;
-export const MAX_PROJECT_IMPORTANCE = 10;
+// Project priority limits
+export const MIN_PROJECT_PRIORITY = 1;
+export const MAX_PROJECT_PRIORITY = 10;

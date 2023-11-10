096c2e323d4b8425fdfc586130860ed2948dc3c7
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-10 20:56:18 +0100

    Add range checks for the priorities

--- TODOList/src/Scripts/Constants/limits.js
+++ TODOList/src/Scripts/Constants/limits.js
@@ -14,3 +14,22 @@ export const MAX_TASK_DESCRIPTION_LENGTH = 1000;
 // Task priority limits
 export const MIN_TASK_PRIORITY = 1;
 export const MAX_TASK_PRIORITY = 10;
+
+
+
+
+
+
+
+
+if (MIN_PROJECT_PRIORITY > MAX_PROJECT_PRIORITY) {
+  throw new RangeError(
+    "Min project priority must not be higher than max priority"
+  );
+}
+
+if (MIN_TASK_PRIORITY > MAX_TASK_PRIORITY) {
+  throw new RangeError(
+    "Min task priority must not be higher than max priority"
+  );
+}

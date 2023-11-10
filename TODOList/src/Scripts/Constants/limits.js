921a5cb4785f8875df3d7f461fbae30b5cead1cd
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-10 20:54:52 +0100

    Add constants for the Task Class

--- TODOList/src/Scripts/Constants/limits.js
+++ TODOList/src/Scripts/Constants/limits.js
@@ -5,3 +5,12 @@ export const MAX_PROJECT_DESCRIPTION_LENGTH = 1000;
 // Project priority limits
 export const MIN_PROJECT_PRIORITY = 1;
 export const MAX_PROJECT_PRIORITY = 10;
+
+
+// Task properties char limits
+export const MAX_TASK_TITLE_LENGTH = 80;
+export const MAX_TASK_DESCRIPTION_LENGTH = 1000;
+
+// Task priority limits
+export const MIN_TASK_PRIORITY = 1;
+export const MAX_TASK_PRIORITY = 10;

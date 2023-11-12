0ca11d9f399d094b90eb68eda76ce6272850a02a
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-12 19:30:09 +0100

    Rewrite Footer in React

--- TODOList/src/Scripts/Shared/Footer.js
+++ TODOList/src/Scripts/Shared/Footer.js
@@ -1,22 +1,23 @@
-const items = [
+import React from "react";
+
+const footerItems = [
   'First item',
   'Second item',
   'Test'
 ]
 
 function Footer() {
-  const footer   = document.createElement("footer");
-  const fragment = document.createDocumentFragment();
+  const items = footerItems.map((item, id) => (
+    <div key={id}>
+      {item}
+    </div>)
+  );
 
-  items.map(item => {
-    const element = document.createElement('div');
-    element.innerText = item;
-    fragment.appendChild(element);
-  })
-
-  footer.appendChild(fragment);
-
-  return footer;
+  return (
+    <footer>
+      {items}
+    </footer>
+  );
 }
 
 export default Footer;

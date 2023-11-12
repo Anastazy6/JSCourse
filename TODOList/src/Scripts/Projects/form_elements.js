b802514b1e3ee25eca0cbeb740438317111bb200
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-12 20:50:40 +0100

    Write skeleton for Project creator form_elements
    The skeleton includes raw data (names, types, constraints etc.) for the
    form controls which will be used to add new Projects in the App.
    
    Also includes a type checking mechanism (made for development mode only),
    which may be refactored as a unit test in the future.

---
+++ TODOList/src/Scripts/Projects/form_elements.js
@@ -0,0 +1,57 @@
+import { MAX_PROJECT_DESCRIPTION_LENGTH } from "../Constants/constraints";
+import { MAX_PROJECT_TITLE_LENGTH       } from "../Constants/constraints";
+import { MIN_PROJECT_PRIORITY           } from "../Constants/constraints";
+import { MAX_PROJECT_PRIORITY           } from "../Constants/constraints";
+
+
+// Testing: Consider moving this stuff to a dedicated testing directory
+import VALID_FORM_CONTROLS from "../Constants/form_controls";
+import VALID_INPUT_TYPES   from "../Constants/form_input_types";
+
+const formItems = [
+  {
+    name     : 'Title',
+    type     : ['input', 'text'],
+    maxlength: MAX_PROJECT_TITLE_LENGTH,
+    required : true,
+  }, {
+    name     : 'Description',
+    type     : ['textarea'],
+    maxlength: MAX_PROJECT_DESCRIPTION_LENGTH
+  }, {
+    name     : 'Notes',
+    type     : ['textarea'],
+  }, {
+    name     : 'Priority',
+    type     : ['input', 'number'],
+    min      : MIN_PROJECT_PRIORITY,
+    max      : MAX_PROJECT_PRIORITY,
+    required : true,
+  }
+];
+
+/**
+ * Development mode function for checking whether the input types are valid.
+ *   Add more type checks here whenever the form uses additional controls
+ *   or move this testing stuff to a dedicated testing directory if it grows
+ *   too much
+ */
+function typeCheck () {
+  formItems.forEach(item => {
+    let controlType = item.type[0].toLowerCase();
+    let inputType   = item.type[1].toLowerCase() || null;
+
+    if (!(VALID_FORM_CONTROLS.includes(controlType))) {
+      throw new TypeError(`Invalid form control type: ${controlType}`);
+    }
+
+    if (controlType === 'input' && !(VALID_INPUT_TYPES.includes(inputType))) {
+      throw new TypeError(`Invalid form input type: ${inputType}`);
+    }
+  });
+}
+
+typeCheck();
+
+
+export default formItems

81af86d44d76fbd5bb787a66fbcab801e0ce11c9
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-12 20:48:15 +0100

    Add VALID_FORM_CONTROLS const for type checks

---
+++ TODOList/src/Scripts/Constants/form_controls.js
@@ -0,0 +1,15 @@
+const VALID_FORM_CONTROLS = [
+  "input",
+  "label",
+  "select",
+  "textarea",
+  "button",
+  "fieldset",
+  "legend",
+  "datalist",
+  "output",
+  "option",
+  "optgroup"
+];
+
+export default VALID_FORM_CONTROLS;

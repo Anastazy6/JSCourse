7a89b1dc81f2b6968a77f8c7d30d4247f3cadc85
Kamil Sidor <kamil_sidor@o2.pl>
2023-11-26 20:54:39 +0100

    Rewrite form elements as standalone Components

--- TODOList/src/Scripts/Projects/form_elements.js
+++ TODOList/src/Scripts/Projects/form_elements.js
@@ -1,69 +1,96 @@
+import React  from 'react';
+
 import { MAX_PROJECT_DESCRIPTION_LENGTH } from "../Constants/constraints";
 import { MAX_PROJECT_TITLE_LENGTH       } from "../Constants/constraints";
 import { MIN_PROJECT_PRIORITY           } from "../Constants/constraints";
 import { MAX_PROJECT_PRIORITY           } from "../Constants/constraints";
 
 
-// Testing: Consider moving this stuff to a dedicated testing directory
-import VALID_FORM_CONTROLS from "../Constants/form_controls";
-import VALID_INPUT_TYPES   from "../Constants/form_input_types";
 
-const formItems = [
-  {
-    name    : 'Title',
-    tag     : 'input',
-    required: true,
-    props   : {
-      type     : 'text',
-      maxlength: MAX_PROJECT_TITLE_LENGTH,
-    },
-  }, {
-    name    : 'Description',
-    tag     : 'textarea',
-    required: false,
-    props   : {
-      maxlength: MAX_PROJECT_DESCRIPTION_LENGTH
-    }
-  }, {
-    name    : 'Notes',
-    tag     : 'textarea',
-    required: false,
-  }, {
-    name    : 'Priority',
-    tag     : 'input',
-    required: true,
-    props   : {
-      type     : 'number',
-      min      : MIN_PROJECT_PRIORITY,
-      max      : MAX_PROJECT_PRIORITY,
-    }
-  }
-];
-
-/**
- * Development mode function for checking whether the input types are valid.
- *   Add more type checks here whenever the form uses additional controls
- *   or move this testing stuff to a dedicated testing directory if it grows
- *   too much
- */
-function typeCheck () {
-  formItems.forEach(item => {
-    let controlType = item.tag;
-
-    if (!(VALID_FORM_CONTROLS.includes(controlType))) {
-      throw new TypeError(`Invalid form control type: ${controlType}`);
-    }
-
-    if (controlType === 'input') {
-      let inputType = item.props.type;
-      if (!(VALID_INPUT_TYPES.includes(inputType))) {
-        throw new TypeError(`Invalid form input type: ${inputType}`);
-      }
-    }
-  });
+export function Title ({project, onChange}) {
+  return (
+    <>
+      <label htmlFor='project-title-input'>
+        Title
+      </label>
+      <input 
+        onChange={onChange}
+        value={project.title}
+        id='project-title-input'
+        name='title'
+        type='text'
+        maxLength={MAX_PROJECT_TITLE_LENGTH}
+        required
+      />
+    </>
+  )
 }
 
-//typeCheck();
+export function Description ({project, onChange}) {
+  return (
+    <>
+      <label htmlFor='project-description-input'>
+        description
+      </label>
+      <textarea
+        onChange={onChange}
+        value={project.description}
+        id='project-description-input'
+        name='description'
+        maxLength={MAX_PROJECT_DESCRIPTION_LENGTH}
+      >
+      </textarea>
+    </>
+  )
+}
+
+export function Notes ({project, onChange}) {
+  return (
+    <>
+      <label htmlFor='project-notes-input'>
+        Notes
+      </label>
+      <textarea
+        value={project.notes}
+        onChange={onChange}
+        id='project-description-notes'
+        name='notes'
+      >
+      </textarea>
+    </>
+  )
+}
+
+export function Priority ({project, onChange}) {
+  return(
+    <>
+      <label htmlFor='project-priority-input'>
+        Priority
+      </label>
+      <input 
+        value={project.priority}
+        onChange={onChange}
+        id='project-priority-input'
+        name='priority'
+        min={MIN_PROJECT_PRIORITY}
+        max={MAX_PROJECT_PRIORITY}
+        step='1'
+        required
+      />
+    </>
+  )
+}
+
+export function Submit () {
+  return (
+    <button
+      className='btn btn-outline-lleuad'
+      role='submit'
+    >
+      Create project
+    </button>
+  )
+}
 
 
-export default formItems;
+

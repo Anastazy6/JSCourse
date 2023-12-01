d868ece39408d2763ec6d1b745db60d5733383e3
Kamil Sidor <kamil_sidor@o2.pl>
2023-12-01 18:18:04 +0100

    Begin work on validation for the Project forms

---
+++ TODOList/src/Scripts/Projects/validate.js
@@ -0,0 +1,10 @@
+import { 
+  MAX_PROJECT_TITLE_LENGTH,
+  MAX_PROJECT_DESCRIPTION_LENGTH,
+  MIN_PROJECT_PRIORITY,
+  MAX_PROJECT_PRIORITY 
+} from "../Constants/constraints";
+
+export function validate (project) {
+
+}

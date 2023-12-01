a6ec60cd2d6f2adb34aae1b5eff2a01bcd936319
Kamil Sidor <kamil_sidor@o2.pl>
2023-12-01 18:17:05 +0100

    Use footer for easier access to LS

--- TODOList/src/Scripts/Shared/Footer.js
+++ TODOList/src/Scripts/Shared/Footer.js
@@ -1,15 +1,23 @@
 import React from "react";
 
 const footerItems = [
-  'First item',
-  'Second item',
-  'Test'
+  {
+    text: 'Clear local storage',
+    onClick: (() => localStorage.clear())},
+  {
+    text: 'Delete all projects',
+    onClick: (() => localStorage.removeItem('projects'))
+  },
+  {
+    text: "Reset projects' counter",
+    onCLick: (() => localStorage.setItem('projectId', 0))
+  }
 ]
 
 function Footer() {
   const items = footerItems.map((item, id) => (
-    <div key={id}>
-      {item}
+    <div key={id} onClick={item.onClick}>
+      {item.text}
     </div>)
   );
 

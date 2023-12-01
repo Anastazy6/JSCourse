390e56700031ffd356934e7b6c471ce10329efd8
Kamil Sidor <kamil_sidor@o2.pl>
2023-12-01 18:16:13 +0100

    Move LS access functions to separate file

---
+++ TODOList/src/Scripts/Projects/storage.js
@@ -0,0 +1,65 @@
+export function saveProject (project) { 
+  let newProject = {...project};
+  
+  const projects = getProjects();
+  const isEditingAnOldProject = projects.some(p => p.id === newProject.id);
+
+  if (projects) {    
+    if (isEditingAnOldProject) {
+      editProject(projects, newProject);
+    } else {
+      saveNewProject(projects, newProject);
+    }
+  // Save a new and the very first project while initializing the 'projects' item in LS
+  } else {
+    saveFirstProject(newProject);
+  }
+}
+
+export function getProjectId () {
+  return parseInt(localStorage.getItem('projectId'))
+}
+
+
+const getProjects = () => JSON.parse(localStorage.getItem('projects'));
+
+
+
+function incrementProjectId () {
+  return localStorage.setItem('projectId', getProjectId() + 1);
+}
+
+
+
+
+
+
+
+
+function editProject (projects, newProject) {
+  const nextProjects = projects.map(p => {
+    if (newProject.id === p.id) return newProject;
+
+    return p;
+  })
+  localStorage.setItem('projects', JSON.stringify([...nextProjects]));
+}
+
+
+function saveNewProject (projects, newProject) {
+  localStorage.setItem('projects', JSON.stringify([...projects, newProject]));
+  incrementProjectId();
+}
+
+
+function saveFirstProject (newProject) {
+  localStorage.setItem('projects', JSON.stringify([newProject]));
+  incrementProjectId();
+}
+
+
+// Initialize current project id variable as 0, if it's not present in local storage,
+//   which is the persistent memory for this JS Project
+if (!getProjectId()) {
+  localStorage.setItem('projectId', 0);
+}

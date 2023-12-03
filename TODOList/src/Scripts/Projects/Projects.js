0a17ac1172082e3be7c4eb60864e2b72e96fe15e
Kamil Sidor <kamil_sidor@o2.pl>
2023-12-03 19:07:15 +0100

    Refactor the main Projects component

--- TODOList/src/Scripts/Projects/Projects.js
+++ TODOList/src/Scripts/Projects/Projects.js
@@ -5,6 +5,7 @@ import Header from "../Shared/Header";
 
 import SingleProject  from "./Components/SingleProject";
 import ProjectsHeader from "./Components/ProjectsHeader";
+import NewProject     from "./Components/New-Project";
 
 
 import * as Storage from "./storage";
@@ -13,11 +14,19 @@ import * as Storage from "./storage";
 
 function Projects () {
   const [projects, setProjects] = useState(Storage.getProjects());
+  const [isNewProjectFormVisible, setIsNewProjectFormVisible] = useState(false);
+  
   let  renderedProjects;
 
 
   function refresh () {
     setProjects(Storage.getProjects());
+    setIsNewProjectFormVisible(false);
+  }
+
+
+  function switchView () {
+    setIsNewProjectFormVisible(!isNewProjectFormVisible);
   }
 
 
@@ -26,8 +35,8 @@ function Projects () {
       return (
         <SingleProject
           props={p}
+          onUpdate={refresh}
           key={`project#${p.id}`}
-          refresh={() => refresh()}
         />
       )
     });
@@ -35,27 +44,66 @@ function Projects () {
 
 
   return ( 
-    projects
-    ? (
-      <table id='all-projects'>
-        <ProjectsHeader />
-        <tbody>
-          {renderedProjects}
-        </tbody>
-      </table>
-    ) : (
-      <Header 
-        level={'h3'}
-        text={'You have no Projects yet'}
+    <>
+      <Header level={'h1'} text={'Projects'} />
+      
+      <NewProject 
+        onCreateProject={refresh} 
+        isVisible      ={isNewProjectFormVisible}
       />
-    )
+
+      <ProjectsView 
+        projects        ={projects}
+        renderedProjects={renderedProjects}
+        isVisible       ={!isNewProjectFormVisible}
+      />
+
+      <ViewSwitch 
+        onSwitchView ={switchView}
+        isFormVisible={isNewProjectFormVisible}
+      />
+
+    </>
   )
 }
 
 
-
-
-
-
-
 export default Projects;
+
+
+function ProjectsView ({projects, renderedProjects, isVisible}) {
+  return (
+    <section
+    style={{display: isVisible ? '' : 'none'}}
+  >
+  <Header level={'h2'} text={'Current Projects'} />
+  {projects
+  ? (
+    <table id='all-projects'>
+      <ProjectsHeader />
+      <tbody>
+        {renderedProjects}
+      </tbody>
+    </table>
+  ) : (
+    <Header 
+      level={'h3'}
+      text={'You have no Projects yet'}
+    />
+  )}
+  </section>
+  )
+}
+
+
+function ViewSwitch ({onSwitchView, isFormVisible}) {
+  return (
+    <button
+    className='btn btn-outline-lleuad-lawn'
+    onClick  ={onSwitchView}
+  >
+
+  {isFormVisible ? 'Show projects' : 'Create a new Project'}
+  </button>
+  )
+}

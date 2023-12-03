6e07aa3196cc82bce2feba90be6089c83559e1fc
Kamil Sidor <kamil_sidor@o2.pl>
2023-12-03 19:09:20 +0100

    Refactor New Project form (WIP: bugfix needed)

--- TODOList/src/Scripts/Projects/Components/New-Project.js
+++ TODOList/src/Scripts/Projects/Components/New-Project.js
@@ -12,15 +12,16 @@ import { MIN_PROJECT_PRIORITY } from "../../Constants/constraints";
 
 import * as Storage from '../storage';
 import { isProjectValid } from "../validate";
+import Header from "../../Shared/Header";
 
 
 
-function NewProject() {
+function NewProject({onCreateProject, isVisible}) {
   const [project, setProject] = useState({
     id         : Storage.getProjectId(),
-    title      : 'Title',
-    description: 'Description',
-    notes      : 'Notes',
+    title      : '',
+    description: '',
+    notes      : '',
     priority   : MIN_PROJECT_PRIORITY
   });
 
@@ -38,6 +39,7 @@ function NewProject() {
     if (isProjectValid(project)) {
       Storage.saveProject(project);
     }
+    onCreateProject();
   }
 
 
@@ -52,15 +54,27 @@ function NewProject() {
 
 
   return(
-    <form
-      onSubmit={handleSubmit}
+    <section
+      style={{display: isVisible ? 'grid' : 'none'}}
     >
-      <Title       {...inputProps} />
-      <Description {...inputProps} />
-      <Notes       {...inputProps} />
-      <Priority    {...inputProps} />
-      <Submit />
-    </form>
+      
+      <Header 
+        level='h2'
+        text ='Create a new Project'
+      />
+      
+      <form
+        onSubmit={handleSubmit}
+        className="new-project-form"  
+      >
+        <Title       {...inputProps} />
+        <Priority    {...inputProps} />
+        <Description {...inputProps} />
+        <Notes       {...inputProps} />
+        <Submit />
+      </form>
+
+    </section>
   );
 }
 

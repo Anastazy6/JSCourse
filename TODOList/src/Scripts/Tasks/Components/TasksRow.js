4ba07ea1c6e4fc560b3bb909d3f605a1411027b0
Kamil Sidor <kamil_sidor@o2.pl>
2024-01-05 20:34:06 +0100

    Add coloring for tasks's statuses

--- TODOList/src/Scripts/Tasks/Components/TasksRow.js
+++ TODOList/src/Scripts/Tasks/Components/TasksRow.js
@@ -51,7 +51,9 @@ function TasksRow ({props, onUpdate}) {
           <td>{ getExcerpt(75, task.notes)       }</td>
           <td>{ task.dueDate  }</td>
           <td>{ task.priority }</td>
-          <td>{ task.status   }</td>
+          <td
+            className={`task-status-${task.status}`}
+          >{ task.status   }</td>
           <td>
             <button
               onClick={handleEdit}
